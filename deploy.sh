9c596a56ba52ad7fae418fcfbf2293920560d1f1
Omotayo Ikudayisi <130115684+Glitzzybetty@users.noreply.github.com>
2024-12-28 01:35:58 +0100

    Update deploy.sh
    added readme

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 |-- app.py                    # Main API script
 |-- requirements.txt          # List of Python dependencies
 |-- watch-it_model.pkl        # Machine Learning model
+└── README.md                 # Documentation
