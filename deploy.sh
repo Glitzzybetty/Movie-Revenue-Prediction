c5cfe4084f79dbe557b7a0ff2a526c45b8889bdd
Omotayo Ikudayisi <130115684+Glitzzybetty@users.noreply.github.com>
2024-12-28 01:20:25 +0100

    model deploy bash file

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+/movie-revenue-api
+|-- app.py                    # Main API script
+|-- requirements.txt          # List of Python dependencies
+|-- watch-it_model.pkl        # Machine Learning model
