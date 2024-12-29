b3fc6737accd4eca6fa527b74abacc0a603a64c0
Omotayo Ikudayisi <130115684+Glitzzybetty@users.noreply.github.com>
2024-12-29 01:25:08 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-/movie-revenue-api
-|-- app.py                    # Main API script
-|-- requirements.txt          # List of Python dependencies
-|-- watch-it_model.pkl        # Machine Learning model
-└── README.md                 # Documentation
